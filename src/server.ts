e97f6e12fc1d51a0a5015cdf95f1357a1fa0cd2f
Stoil Yankov <stoil.yankov@scalefocus.com>
2020-02-11 00:13:38 +0200

    Added controller.

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,19 @@
-import bodyParser from 'body-parser';
-import { UserController } from './controllers/userController';
-import express, { Request, Response } from 'express';
-require('dotenv').config();
+import bodyParser from "body-parser";
+import { ApiController } from "./controllers/apiController";
+import express, { Request, Response } from "express";
+require("dotenv").config();
 
 const app: express.Application = express();
 const port: number = +(process.env.PORT || 3000);
 
-const router = express.Router();
-
-var SpotifyWebApi = require('spotify-web-api-node');
-
-// Replace with your redirect URI, required scopes, and show_dialog preference
-// var redirectUri = `http://localhost:3000/`;
-// var scopes = ['user-top-read', 'streaming'];
-// var showDialog = true;
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use("/", UserController);
+app.use("/api", ApiController);
 
 app.listen(port, () => {
-    // Success callback
-    console.log(`Listening at http://localhost:${port}/`);
+  // Success callback
+  console.log(`Listening at http://localhost:${port}/`);
 });
-
